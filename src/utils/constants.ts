c44d7b27f21c30b3cd2c8e40424d040ba86ced8b
Biplob Sutradhar <biplobsd11@gmail.com>
2023-05-07 23:24:38 +0600

    v1.1

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const STORIES_URL = ["https://www.youtube.com/"];
-export const DEFAULT_STATUS_MSG = 'Ready for request';
-export const APP_NAME = 'Youtube Subscriptions Transfer';
-export const VERSION = 'v1.0';
+export const DEFAULT_STATUS_MSG = "Ready for request";
+export const APP_NAME = "Youtube Subscriptions Transfer";
+export const VERSION = "v1.1";
