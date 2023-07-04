2ac125d5aa9c4b02dfde456ca724b1152fdb98e6
Biplob Sutradhar <biplobsd11@gmail.com>
2023-07-04 15:00:04 +0600

    button horizontal

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,26 +4,23 @@
   import CoffeeIcon from "./Coffee_Icon.svelte";
 </script>
 
-<div class="my-2 space-y-1">
-  <div
-    class="mt-5 flex items-center dark:text-slate-100/50 text-slate-700/50 gap-2 justify-center"
+<div class="my-2 gap-2 flex justify-center items-center mb-3">
+  <a
+    class="btn btn-xs normal-case"
+    target="_blank"
+    rel="noreferrer"
+    href={REPO_URL}
   >
-    <img class="w-5 h-5" src={GITHUB_LOGO} alt="Github logo" /><a
-      class="link link-hover link-info"
-      target="_blank"
-      rel="noreferrer"
-      href={REPO_URL}>Fork now</a
-    >
-  </div>
-  <p class="text-xs mb-1 dark:text-slate-100/50 text-slate-700/50 text-center">
-    <a
-      class="link link-hover flex gap-2 items-center"
-      target="_blank"
-      rel="noreferrer"
-      href="https://www.buymeacoffee.com/biplobsd"
-    >
-      <CoffeeIcon />
-      Buy me a coffee</a
-    >
-  </p>
+    <img class="w-4 h-4" src={GITHUB_LOGO} alt="Github logo" />
+    Learn more
+  </a>
+  <a
+    class="btn btn-xs normal-case"
+    target="_blank"
+    rel="noreferrer"
+    href="https://www.buymeacoffee.com/biplobsd"
+  >
+    <CoffeeIcon />
+    Buy me a coffee</a
+  >
 </div>
