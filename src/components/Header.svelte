34090d8747b66485b7f56a884452a0ac8be632e4
Biplob Sutradhar <biplobsd11@gmail.com>
2023-07-08 21:28:17 +0600

    abbreviation mode

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -21,14 +21,18 @@
   <div class="flex justify-center items-center gap-1">
     <div class="w-12 h-full flex justify-center items-center flex-col">
       <img src={icon48} alt="Logo" class="mx-auto" />
-      {#key localMode}
-        <div
-          in:blur
-          class="h-fit text-[8px]/[8px] text-center uppercase font-mono"
-        >
-          {localMode}
-        </div>
-      {/key}
+      <abbr
+        title={`Working mode. Learn more by pressing the "Learn more" button below.`}
+      >
+        {#key localMode}
+          <div
+            in:blur
+            class="h-fit text-[8px]/[8px] text-center uppercase font-mono"
+          >
+            {localMode}
+          </div>
+        {/key}
+      </abbr>
     </div>
     <span class="text-sm">{APP_NAME}</span>
   </div>
@@ -37,3 +41,11 @@
     <ThemeSwitch />
   </div>
 </div>
+
+<style>
+  abbr[title] {
+    border-bottom: none !important;
+    cursor: inherit !important;
+    text-decoration: none !important;
+  }
+</style>
