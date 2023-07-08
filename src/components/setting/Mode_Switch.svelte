1bdd8eeae3ccf461e7ad99e5336887842bcda407
Biplob Sutradhar <biplobsd11@gmail.com>
2023-07-08 21:06:00 +0600

    API mode only for chrome browser

--- src/components/setting/Mode_Switch.svelte
+++ src/components/setting/Mode_Switch.svelte
@@ -2,13 +2,20 @@
   import { MODE_DEFAULT } from "src/utils/default";
   import { modeWritable, type MODE } from "src/utils/storage";
   import { onMount } from "svelte";
+  import { detect } from "detect-browser";
 
   let localMode: MODE = MODE_DEFAULT;
+  let isChrome = false;
 
   onMount(() => {
     modeWritable.subscribe((mode) => {
       localMode = mode;
     });
+
+    const browser = detect();
+    if (browser && browser.name === "chrome") {
+      isChrome = true;
+    }
   });
 </script>
 
@@ -31,8 +38,12 @@
 </div>
 <div class="form-control !py-1 !my-0 hover:bg-base-200 rounded-md">
   <label class="label !py-0 !my-0 cursor-pointer">
-    <span class="label-text">API</span>
+    <span class="label-text"
+      >API <span class="text-xs text-info">(Only for chrome browser)</span
+      ></span
+    >
     <input
+      disabled={!isChrome}
       type="radio"
       name="radio-10"
       class="radio radio-xs checked:bg-blue-500"
--- src/components/setting/api/API_Delay.svelte
+++ src/components/setting/api/API_Delay.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
-  import { API_REQ_DELAY_DEFAULT } from "src/utils/default";
+  import { API_REQ_DELAY_DEFAULT, MODE_DEFAULT } from "src/utils/default";
   import { apiReqDelayWritable } from "src/utils/storage";
+  import { modeWritable, type MODE } from "src/utils/storage";
   import { onMount } from "svelte";
   import toast from "svelte-french-toast";
+  let localMode: MODE = MODE_DEFAULT;
 
   let delay = API_REQ_DELAY_DEFAULT;
 
   onMount(() => {
     apiReqDelayWritable.subscribe((value) => (delay = value));
+
+    modeWritable.subscribe((mode) => {
+      localMode = mode;
+    });
   });
 </script>
 
@@ -15,6 +21,7 @@
   <label class="label">
     <span class="label-text">API delay per request</span>
     <input
+      disabled={localMode !== "api"}
       type="number"
       placeholder="ms"
       value={delay}
