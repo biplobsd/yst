a410c7fbb02625633e1c78a81a1e7373016bc7e5
Biplob Sutradhar <biplobsd11@gmail.com>
2023-07-08 16:01:33 +0600

    5 item view

--- src/components/api/Data.svelte
+++ src/components/api/Data.svelte
@@ -1,22 +1,67 @@
 <script lang="ts">
-  import { blur } from "svelte/transition";
+  import type { SubscriptionsList } from "src/utils/schema";
+  import { subscriptionsWritable } from "src/utils/storage";
+  import { onMount } from "svelte";
+  import { blur, slide } from "svelte/transition";
   export let subscriptionCount: number;
+  let subscriptionsList: SubscriptionsList = [];
+
+  onMount(() => {
+    subscriptionsWritable.subscribe((value) => (subscriptionsList = value));
+  });
 </script>
 
 <div class="font-bold">Data</div>
 <div
   class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
 >
-  <input type="checkbox" class="peer !min-h-8" />
+  <input type="checkbox" class="peer !min-h-8" checked />
   <div
-    class="!min-h-8 !py-0 flex items-center collapse-title text-sm bg-success/70 text-black/70 tracking-wider font-sans"
+    class="!min-h-8 !py-0 flex items-center collapse-title text-sm tracking-wider font-sans"
   >
     Subscriptions:
     {#key subscriptionCount}
       <span in:blur>{subscriptionCount}</span>
     {/key}
   </div>
-  <div class="collapse-content bg-success/60 peer-checked:py-2">
-    Data will show here
+  <div class="collapse-content peer-checked:py-2 space-y-2">
+    <div class="scrollbar-style overflow-auto w-60 min-w-full md:min-w-0">
+      <table class="table table-xs table-pin-cols">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Channel ID</th>
+            <th>ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each subscriptionsList.slice(0, 5) as { title, channelId, id }}
+            <tr class="hover">
+              <td>{title}</td>
+              <td>{channelId}</td>
+              <td>{id}</td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
+    <div class="alert alert-info py-1 gap-1 flex justify-center item-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+        />
+      </svg>
+
+      <span class=" w-full text-start">5 item view</span>
+    </div>
   </div>
 </div>
