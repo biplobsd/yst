19e5df0b83c905ed385aee3efcc4abff6dc97822
Biplob Sutradhar <biplobsd11@gmail.com>
2024-04-24 21:33:13 +0600

    Update dependencies and add documentation links

---
+++ src/components/External_Link.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { ExternalLinkIcon } from "lucide-svelte";
+  </script>
+  
+  <div class="indicator group">
+    <ExternalLinkIcon
+      class="indicator-item badge badge-xs invisible group-hover:visible"
+    />
+    <div class="place-items-center">
+      <slot />
+    </div>
+  </div>
--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  import { REPO_URL } from "src/utils/constants";
   import CoffeeIcon from "./icons/Coffee_Icon.svelte";
   import { isDarkThemeWritable } from "src/utils/storage";
   import { onMount } from "svelte";
   import GithubMark from "src/assets/icons/github-mark.png";
   import GithubMarkWhite from "src/assets/icons/github-mark-white.png";
   import { docs } from "src/utils/docs";
+  import ExternalLink from "./External_Link.svelte";
 
   let isLight = false;
 
@@ -17,27 +17,30 @@
 </script>
 
 <div class="my-2 gap-2 flex justify-center items-center mb-3">
-  <a
-    class="btn btn-xs normal-case"
-    target="_blank"
-    rel="noreferrer"
-    href={docs.README}
-  >
-    <img
-      class="w-4 h-4"
-      src={isLight ? GithubMark : GithubMarkWhite}
-      alt="Github logo"
-    />
-
-    Learn more
-  </a>
-  <a
-    class="btn btn-xs normal-case"
-    target="_blank"
-    rel="noreferrer"
-    href="https://www.buymeacoffee.com/biplobsd"
-  >
-    <CoffeeIcon />
-    Buy me a coffee</a
-  >
+  <ExternalLink>
+    <a
+      class="btn btn-xs normal-case"
+      target="_blank"
+      rel="noreferrer"
+      href={docs.README}
+    >
+      <img
+        class="w-4 h-4"
+        src={isLight ? GithubMark : GithubMarkWhite}
+        alt="Github logo"
+      />
+      Learn more
+    </a>
+  </ExternalLink>
+  <ExternalLink>
+    <a
+      class="btn btn-xs normal-case"
+      target="_blank"
+      rel="noreferrer"
+      href="https://www.buymeacoffee.com/biplobsd"
+    >
+      <CoffeeIcon />
+      Buy me a coffee</a
+    >
+  </ExternalLink>
 </div>
